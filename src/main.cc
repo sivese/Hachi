1f95da36218dae37dd9077447a861a1cfc7214bc
sivese <driversivese@gmail.com>
2025-06-06 14:50:56 -0700

    building basic module

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,19 @@
 #include <stdio.h>
+#include <emscripten.h>
+
+extern "C" {
+    EMSCRIPTEN_KEEPALIVE void reset() {
+        printf("Resetting...\n");
+    }
+
+    EMSCRIPTEN_KEEPALIVE void pause() {
+        printf("Pausing...\n");
+    }
+
+    EMSCRIPTEN_KEEPALIVE void load_rom() {
+        printf("Loading ROM...\n");
+    }
+}
 
 int main() {
     printf("Hello World!\n");
