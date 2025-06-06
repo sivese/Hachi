b953fd450d57d4bf8d06aca6c72fd26050b883b1
sivese <driversivese@gmail.com>
2025-06-06 14:43:19 -0700

    build script complete

--- script/build.bat
+++ script/build.bat
@@ -3,17 +3,24 @@ REM Check if emsdk exists, if not initialize the submodule
 if not exist emsdk (
     echo Initializing Emscripten SDK...
     git submodule update --init --recursive
+
     cd emsdk
-    call emsdk.bat install latest
-    call emsdk.bat activate latest
+    call emsdk install latest
+    call emsdk activate latest
     cd ..
 )
 
 REM Source the Emscripten environment
-call emsdk\emsdk_env.bat
+call emsdk/emsdk_env.bat
 
 REM Build the project
-emcmake cmake -B build
-cmake --build build
+call emcmake cmake -B build
 
-echo Build complete. Run 'cd build && python -m http.server 8080' to test
+echo Building camke script...
+call cmake --build build
+
+echo Build complete. Starting local server...
+start http://localhost:8080
+
+cd build
+python -m http.server 8080
--- script/init.bat
+++
@@ -1,6 +0,0 @@
-echo Initializing Emscripten SDK...
-git submodule update --init --recursive
-cd emsdk
-call emsdk.bat install latest
-call emsdk.bat activate latest
-cd ..
